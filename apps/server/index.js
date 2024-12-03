16567cee655814039fca0ac5ebf88a248dc75c06
Ndubuishi Ulenu <nduulenu@gmail.com>
2024-12-03 21:03:52 +0100

    deploy configuration added_hotfix-9

--- apps/server/index.js
+++ apps/server/index.js
@@ -31,24 +31,24 @@ app.use('/api', designRoutes);
 app.use(errorHandler);
 
 // Serve frontend
-if (process.env.NODE_ENV === 'production') {
  
 // app.use(express.static(path.join(__dirname, '../../apps/client/build')));
 // app.get('*', (req, res) => {
 //   res.sendFile(path.join(__dirname, '../../apps/client/build/index.html'));
 // });
 // }
-app.use(express.static(path.join(__dirname, '../client/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-}
+
 
 export default app
