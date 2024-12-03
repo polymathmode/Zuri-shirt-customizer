3b7edeb188bf6e05b95d50a66bd5faa9a0c95465
Ndubuishi Ulenu <nduulenu@gmail.com>
2024-12-03 19:49:28 +0100

    deploy configuration added_hotfix4

--- apps/server/index.js
+++ apps/server/index.js
@@ -32,11 +32,11 @@ app.use(errorHandler);
 
 // Serve frontend
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, './client/build')));
-  
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/build/index.html'));
-  });
+ 
+app.use(express.static(path.join(__dirname, '../../apps/client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../apps/client/build/index.html'));
+});
 }
 
 const PORT = process.env.PORT || 5000;
